e2e4138d745bf53c02860fa9b796c77f9aec50aa
lusqua <lucasaguiar.floripa@gmail.com>
2023-05-04 22:48:08 -0300

    Feat. bypass if status path

--- src/api/middlewares/tokenCheck.js
+++ src/api/middlewares/tokenCheck.js
@@ -1,16 +1,19 @@
 const config = require('../../config/config')
 
 function tokenVerification(req, res, next) {
+    // get request path
+    const path = req.path
+
     const bearer = req.headers.authorization
     const token = bearer?.slice(7)?.toString()
-    if (!token) {
+    if (!token && path !== '/status') {
         return res.status(403).send({
             error: true,
             message: 'no bearer token header was present',
         })
     }
 
-    if (config.token !== token) {
+    if (config.token !== token && path !== '/status') {
         return res
             .status(403)
             .send({ error: true, message: 'invalid bearer token supplied' })
